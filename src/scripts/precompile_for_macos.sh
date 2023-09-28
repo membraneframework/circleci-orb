bb6e09188b2baef26dda6527b2a5e87a81b7f292
Noarkhh <jakub.pryc@gmail.com>
2023-09-28 17:05:35 +0200

    Adjust for M1 install location

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -1,4 +1,8 @@
 artifact_name="${PACKAGE_NAME}_macos_${ARCHITECTURE}"
+case $ARCHITECTURE in 
+    m1) brew_prefix="/opt/homebrew" ;;
+    intel) brew_prefix="/usr/local" ;;
+esac    
 mkdir -p ~/project/workspace/$artifact_name/include
 mkdir -p ~/project/workspace/$artifact_name/lib
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -8,8 +12,8 @@ if [[ ! $EXPECTED_VERSION =~ ^($real_version|"not a tag") ]]; then
     echo "Version passed via tag not matching installed version"
     exit 1
 fi    
-cp -r /usr/local/include/* ~/project/workspace/$artifact_name/include
-cd /usr/local/lib || exit 1
+cp -r "${brew_prefix}/include/*" ~/project/workspace/$artifact_name/include
+cd "${brew_prefix}/lib" || exit 1
 for f in *.dylib; do
     install_name_tool -id "@rpath/$f" $f
     cp -a "$(readlink $f)" ~/project/workspace/$artifact_name/lib
