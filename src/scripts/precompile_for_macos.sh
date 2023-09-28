23e4be1fc53679577e01abc32c57ee40ca4f7c7b
Noarkhh <jakub.pryc@gmail.com>
2023-09-28 16:39:18 +0200

    Add precompilation job for M1 architecture

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -1,10 +1,10 @@
-artifact_name="${PACKAGE_NAME}_macos_intel"
+artifact_name="${PACKAGE_NAME}_macos_${ARCHITECTURE}"
 mkdir -p ~/project/workspace/$artifact_name/include
 mkdir -p ~/project/workspace/$artifact_name/lib
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install $PACKAGE_NAME
 real_version="v$(brew list --versions | grep -w $PACKAGE_NAME | cut -d ' ' -f 2)"
-if [[ ! $EXPECTED_VERSION =~ ^($real_version|"null") ]]; then
+if [[ ! $EXPECTED_VERSION =~ ^($real_version|"") ]]; then
     echo "Version passed via tag not matching installed version"
     exit 1
 fi    
