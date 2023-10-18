6ed17a77b0f6ed8d23b9e5272d3547d37fdde06f
Noarkhh <jakub.pryc@gmail.com>
2023-10-18 13:17:25 +0200

    Codesign on arm, fix embedded absolute paths

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -10,13 +10,14 @@ brew install $PACKAGE_NAME
 real_version="v$(brew list --versions | grep -w $PACKAGE_NAME | cut -d ' ' -f 2)"
 if [[ ! $EXPECTED_VERSION =~ ^($real_version|"no check")$ ]]
 then
-    echo "Version passed via tag: $(EXPECTED_VERSION) not matching installed version: $($real_version)"
+    echo "Version passed via tag: $EXPECTED_VERSION not matching installed version: $real_version"
     exit 1
 fi    
 cp -r ${brew_prefix}/include/* ~/project/workspace/$artifact_name/include
-cd ${brew_prefix}/lib || exit 1
-for f in *.dylib
+for f in ${brew_prefix}/lib/*.dylib
 do
-    if [[ $ARCHITECTURE == intel ]]; then install_name_tool -id "@rpath/$f" $f; fi
+    install_name_tool -id "@rpath/$(basename $f)" $f
+    otool -L $f | tail -n +3 | cut -d ' ' -f 1 | grep "^${brew_prefix}" | while read -r line; do install_name_tool -change $line @rpath/$(basename $line) $f; done
+    [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
     cp -a "$(readlink $f)" ~/project/workspace/$artifact_name/lib
 done
