6d7e25147b0068a7ff067af3b322698439fe0e5f
Noarkhh <jakub.pryc@gmail.com>
2023-10-18 15:56:10 +0200

    Codesign only on non-symlinks

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -17,8 +17,11 @@ cp -r ${brew_prefix}/include/* ~/project/workspace/$artifact_name/include
 cd "${brew_prefix}"/lib || exit 1
 for f in *.dylib
 do
-    install_name_tool -id "@rpath/$f" $f
-    otool -L $f | tail -n +3 | cut -d ' ' -f 1 | ( grep "^${brew_prefix}" || [ "$?" == "1" ] ) | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
-    [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
+    if [! -L $f]
+    then
+        install_name_tool -id "@rpath/$f" $f
+        otool -L $f | tail -n +3 | cut -d ' ' -f 1 | ( grep "^${brew_prefix}" || [ "$?" == "1" ] ) | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
+        [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
+    fi
     cp -a "$(readlink $f)" ~/project/workspace/$artifact_name/lib
 done
