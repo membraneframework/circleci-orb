13665b98cbd4f7c8ea718f2d091bc53704428cfb
Noarkhh <jakub.pryc@gmail.com>
2023-10-18 15:32:37 +0200

    Apply fixes

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -14,10 +14,11 @@ then
     exit 1
 fi  
 cp -r ${brew_prefix}/include/* ~/project/workspace/$artifact_name/include
-for f in "${brew_prefix}"/lib/*.dylib
+cd "${brew_prefix}"/lib || exit 1
+for f in *.dylib
 do
-    install_name_tool -id "@rpath/$(basename $f)" $f
+    install_name_tool -id "@rpath/$f" $f
     otool -L $f | tail -n +3 | cut -d ' ' -f 1 | ( grep "^${brew_prefix}" || [ "$?" == "1" ] ) | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
     [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
-    cp -a "$(readlink -f $f)" ~/project/workspace/$artifact_name/lib
+    cp -a "$(readlink $f)" ~/project/workspace/$artifact_name/lib
 done
