c9584c10c717533013d49afe19ed183276e29205
Noarkhh <jakub.pryc@gmail.com>
2023-10-18 13:41:28 +0200

    Fix reading symlinks

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -19,5 +19,5 @@ do
     install_name_tool -id "@rpath/$(basename $f)" $f
     otool -L $f | tail -n +3 | cut -d ' ' -f 1 | grep "^${brew_prefix}" | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
     [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
-    cp -a "$(readlink $f)" ~/project/workspace/$artifact_name/lib
+    cp -a "$(readlink -f $f)" ~/project/workspace/$artifact_name/lib
 done
