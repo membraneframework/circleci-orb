cdd1f556fc53633228ca2fa417edfdde13fb40bd
Noarkhh <jakub.pryc@gmail.com>
2023-10-20 10:53:33 +0200

    Add more comments

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -21,11 +21,15 @@ do
     f=~/project/workspace/$artifact_name/lib/$l
     if [ ! -L $f ]
     then
+        # Problem: libraries installed by brew have load commands in them that use absolute paths. 
+        # This renders them unusable in any other location other than the one they were installed in.
+        # Solution: 
+
         # update the LC_ID_DYLIB command of current library so that it's name is a relative path, 
         # not absolute, so that it can be located during linking
         install_name_tool -id "@rpath/$(basename $f)" $f
-        # update all LC_LOAD_DYLIB commands of current library so that their names are also 
-        # relative paths, so that their dependencies can be located
+        # update LC_LOAD_DYLIB commands that refer to dependencies of current library that have been 
+        # installed by brew so that their names are also relative paths, so that the dependencies can be located during linking
         otool -L $f | tail -n +3 | awk -F" " '{print $1}' | ( grep "^${brew_prefix}" || [ "$?" == "1" ] ) | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
         # if compiled on arm architecture then the libraries will be codesigned on `brew install`
         # and need to be resigned, because the previous changes invalidated the signature
