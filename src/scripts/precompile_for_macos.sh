451c84a5b2af13c97454bf591d6ebc5e9501844f
Noarkhh <jakub.pryc@gmail.com>
2023-10-20 10:42:27 +0200

    Add comments

--- src/scripts/precompile_for_macos.sh
+++ src/scripts/precompile_for_macos.sh
@@ -21,8 +21,14 @@ do
     f=~/project/workspace/$artifact_name/lib/$l
     if [ ! -L $f ]
     then
+        # update the LC_ID_DYLIB command of current library so that it's name is a relative path, 
+        # not absolute, so that it can be located during linking
         install_name_tool -id "@rpath/$(basename $f)" $f
+        # update all LC_LOAD_DYLIB commands of current library so that their names are also 
+        # relative paths, so that their dependencies can be located
         otool -L $f | tail -n +3 | awk -F" " '{print $1}' | ( grep "^${brew_prefix}" || [ "$?" == "1" ] ) | while read -r line; do install_name_tool -change $line "@rpath/$(basename $line)" $f; done
+        # if compiled on arm architecture then the libraries will be codesigned on `brew install`
+        # and need to be resigned, because the previous changes invalidated the signature
         [[ $ARCHITECTURE == arm ]] && codesign --sign - --force --preserve-metadata=entitlements,requirements,flags,runtime $f
     fi
 done
